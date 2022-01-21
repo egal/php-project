cde1beb505ef0819aee6f30b194a6f96441395ec
Ирина Крылова <krylova@sputnikfund.ru>
2022-01-21 16:45:25 +0700

    add some traits

---
+++ server/backend/app/egal/FieldStringFormatTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\egal;
+
+trait FieldStringFormatTrait
+{
+    protected ?string $alpha;
+    protected ?string $alphaDash;
+    protected ?string $alphaNum;
+    protected ?string $endsWith;
+    protected ?string $filled;
+}
