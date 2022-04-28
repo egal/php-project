fdcc4c4747f6ce9c9bbf90454265d5a62215d236
Аня <Sytuagina@sputnikfund.ru>
2022-04-28 14:37:33 +0700

    Update: add filter applier test.

--- server/main-service/egal/tests/Core/Rest/Filter/FilterApplierTest.php
+++ server/main-service/egal/tests/Core/Rest/Filter/FilterApplierTest.php
@@ -5,6 +5,10 @@ namespace Egal\Tests\Core\Rest\Filter;
 use Egal\Core\Database\Metadata\Field as FieldMetadata;
 use Egal\Core\Database\Metadata\Model as ModelMetadata;
 use Egal\Core\Database\Model;
+use Egal\Core\Rest\Filter\Combiner;
+use Egal\Core\Rest\Filter\Condition;
+use Egal\Core\Rest\Filter\Field;
+use Egal\Core\Rest\Filter\Operator;
 use Egal\Core\Rest\Filter\Query as FilterQuery;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use PHPUnit\Framework\TestCase;
@@ -13,15 +17,24 @@ class FilterApplierTest extends TestCase
 {
     public function filterApplierDataProvider(): array
     {
-        return [];
+        $field = new Field('name');
+        $field->setRelation('category');
+
+        return [
+            [
+                FilterQuery::make([
+                    Condition::make('field', Operator::Equals, null, Combiner::And),
+                ])
+            ]
+        ];
     }
 
     /**
      * @dataProvider filterApplierDataProvider()
      */
-    public function testFilterApplier(FilterQuery $filterQuery, string $expectedSql)
+    public function testFilterApplier(FilterQuery $filterQuery, string $expectedSql = '')
     {
-
+        dump(ModelTestProduct::filter($filterQuery)->toSql());
     }
 }
 
